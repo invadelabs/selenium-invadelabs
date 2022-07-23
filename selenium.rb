652e27ca932529d7308f8176083fb39af18f5ba5
Drew Holt <17417299+drew-holt@users.noreply.github.com>
2022-07-23 01:43:08 -0600

    update to ruby@2.7.6 (#27)

--- selenium.rb
+++ selenium.rb
@@ -25,7 +25,7 @@ def setup(browser_name, url)
   @driver = Selenium::WebDriver.for(
     :remote,
     url: url.to_s,
-    desired_capabilities: caps
+    capabilities: caps
   )
 end
 
@@ -40,6 +40,7 @@ def compressimage(filename)
   image_optim = ImageOptim.new(skip_missing_workers: true,
                                verbose: false,
                                optipng: { level: 6 },
+                               oxipng: false,
                                pngcrush: false,
                                jpegtran: false,
                                pngout: false,
